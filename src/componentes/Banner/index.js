68560f86b9753e473d38d0266099465035c11887
Erick Alcântara Silvério <ericksilverio39@gmail.com>
2023-07-05 13:27:03 -0300

    Add files via upload

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="O banner principal do Órgano" />
+    </header>
+  );
+}
+
+export default Banner;
